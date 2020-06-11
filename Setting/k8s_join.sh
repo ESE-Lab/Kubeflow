c110bd2e949c0cb0a1f41aff5e6fb21e83d6b4ed
hochan222 <hochan049@gmail.com>
2020-06-11 23:36:03 +0900

    [FRESH] token

--- Setting/k8s_join.sh
+++ Setting/k8s_join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.0.34:6443 --token a8e9v6.clt98ivas0tq8lln --discovery-token-ca-cert-hash sha256:f5e19861b08e3effdf1e35158221530a90db216265f0f2a36df71664271ff11d
+kubeadm join 192.168.0.34:6443 --token afifdb.qq6l90ail9b4o6lu --discovery-token-ca-cert-hash sha256:f5e19861b08e3effdf1e35158221530a90db216265f0f2a36df71664271ff11d
